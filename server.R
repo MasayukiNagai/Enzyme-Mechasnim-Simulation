2a766c9642b58d504bfc4f4fc8a59415ea4d7a08
Moon <masayukinagai_2022@depauw.edu>
2019-06-28 16:29:03 -0400

    changed layouts of Pt graphs

--- server.R
+++ server.R
@@ -8,6 +8,7 @@ source("lambertMMsd.R")
 source("plot_lambert.R")
 source("plot_lambert2.R")
 source("plot_lambertMM.R")
+source("calc_slopes.R")
 
 server = function(input, output, session) {
 
@@ -61,11 +62,30 @@ server = function(input, output, session) {
     })
     
     #Page2 P-t graph 1
+    output$instruction_Pt = renderUI({
+        includeHTML("instruction_Pt.html")
+    })
+    
     substrates_1 = character()
-    values_1 = reactiveValues(df = data.frame("substrates" = substrates_1))
+    slopes_1 = character()
+    values_1 = reactiveValues(df = data.frame("substrates" = substrates_1, "slopes" = slopes_1))
     newEntry = observeEvent(input$add_s_1, {
-        substrates_new = c(values_1$df$substrates, input$s_1)
-        values_1$df = data.frame("substrates" = substrates_new)
+        if(input$s_1 %in% values_1$df$substrates){
+            substrates_new = values_1$df$substrates
+        }
+        else{
+            substrates_new = c(values_1$df$substrates, input$s_1)
+        }
+        e = as.numeric(input$e_1)
+        time = input$time_1
+        slopes_new = unlist(lapply(substrates_new, calc_slopes, "e" = e, "time" = time))
+        if(e == 1 * 10^(-1) || e == 2 * 10^(-2)){
+            values_1$df = data.frame("substrates" = substrates_new, "slopes" = formatC(slopes_new, format = "f", digits = 3))
+        }
+        else{
+            values_1$df = data.frame("substrates" = substrates_new, "slopes" = formatC(slopes_new, format = "e", digits = 2))
+        }
+        
     })
     
     observe({
@@ -102,32 +122,85 @@ server = function(input, output, session) {
     })
     
     output$table_Pt_1 = renderTable({
-        
+        if(length(values_1$df$substrates) < 10 && length(values_1$df$substrates) > 0){
+            values_1$df[1:length(values_1$df$substrates), c("substrates", "slopes")]
+        }
+        else if (length(values_1$df$substrates) > 10){
+            values_1$df[1:10,c("substrates", "slopes")]
+        }
+        else{
+            values_1$df
+        }
+    }, striped = TRUE, bordered = TRUE, align = "c", width = "200px" )
+    
+    #Page3 P-t graph 1
+    output$instruction_Pt_2 = renderUI({
+        includeHTML("instruction_Pt.html")
     })
-    
-
-    
-    #Page3 P-t graph 2
     substrates_2 = character()
-    values_2 = reactiveValues(df = data.frame("substrates" = substrates_2))
+    slopes_2 = character()
+    values_2 = reactiveValues(df = data.frame("substrates" = substrates_2, "slopes" = slopes_2))
     newEntry = observeEvent(input$add_s_2, {
-        substrates_new = c(values_2$df$substrates, input$s_2)
-        values_2$df = data.frame("substrates" = substrates_new)
+        if(input$s_2 %in% values_2$df$substrates){
+            substrates_new = values_2$df$substrates
+        }
+        else{
+            substrates_new = c(values_2$df$substrates, input$s_2)
+        }
+        e = as.numeric(input$e_2)
+        time = input$time_2
+        slopes_new = unlist(lapply(substrates_new, calc_slopes, "e" = e, "time" = time))
+        if(e == 1 * 10^(-1) || e == 2 * 10^(-2)){
+            values_2$df = data.frame("substrates" = substrates_new, "slopes" = formatC(slopes_new, format = "f", digits = 3))
+        }
+        else{
+            values_2$df = data.frame("substrates" = substrates_new, "slopes" = formatC(slopes_new, format = "e", digits = 2))
+        }
+
     })
-    
+
+    observe({
+        concentration = as.numeric(input$e_2)
+        if(concentration == 1 * 10^(-1)){
+            fixed_time = 10
+        }
+        else if(concentration == 2 * 10^(-2)){
+            fixed_time = 20
+        }
+        else if(concentration == 1 * 10^(-4)){
+            fixed_time = 4000
+        }
+        else if(concentration == 1 * 10^(-5)){
+            fixed_time = 10000
+        }
+        else{
+            fixed_time = 100
+        }
+        updateSliderInput(session, "time_2", value = fixed_time)
+    })
+
     spectrum_2 = reactive({
-        out = lambertMMsd(file = values_2$df,
-                        "e" = input$e_2,
-                        # "km" = input$km_2,
-                        # "k2" = input$k2_2,
-                        "time" = input$time_2,
-                        "sd" = 0.05)
+        out = lambertMM(file = values_2$df,
+                        "e" = as.numeric(input$e_2),
+                        "time" = input$time_2)
     })
-    
+
     output$graph_Pt_2 = renderPlot({
         plot_lambert2(file = spectrum_2(),
-                     display_theoretical_values = input$theory_2)
+                      display_theoretical_values = input$theory_2)
     })
+
+    output$table_Pt_2 = renderTable({
+        if(length(values_2$df$substrates) < 10 && length(values_2$df$substrates) > 0){
+            values_2$df[1:length(values_2$df$substrates), c("substrates", "slopes")]
+        }
+        else if (length(values_2$df$substrates) > 10){
+            values_2$df[1:10,c("substrates", "slopes")]
+        }
+        else{
+            values_2$df
+        }
+    }, striped = TRUE, bordered = TRUE, align = "c", width = 300 )
     
     #Page 4 P-t and MM 1
     substrates_3 = character()
@@ -139,7 +212,7 @@ server = function(input, output, session) {
     
     spectrum_3 = reactive({
         out = lambertMM(file = values_3$df,
-                         "e" = input$e_3 * 10^(-6),
+                         "e" = as.numeric(input$e_3),
                          "time" = input$time_3,
                          "sd" = 0)
     })
@@ -150,7 +223,7 @@ server = function(input, output, session) {
     })
     
     output$graph_MM_3 = renderPlot({
-        plot_lambertMM2(file = spectrum_3(),
+        plot_lambertMM(file = spectrum_3(),
                         display_theoretical_values = input$theory_3)
     })
 }
--- ui.R
+++ ui.R
@@ -42,58 +42,72 @@ ui<-fluidPage(
                          
                          tabPanel("P-t Graph 1",
                                   sidebarPanel(
-                                    selectInput("e_1", "[E] (M)",
-                                                choices = list("0.1 M" = 1 * 10^(-1), "0.02 M" = 2 * 10^(-2), "100 μM" = 100 * 10^(-6), "10 μM" = 10 * 10^(-6)),
-                                                selected = 2 * 10^(-2)),
-                                    sliderInput("s_1", "[S] (M)",
-                                                min = 0, max = 10, value = 1, step = 0.1),
-                                    actionButton("add_s_1", "Do another experiment with a different value of [S]"),
-                                    hr(),
-                                    # sliderInput("km_1", "Km (M)",
-                                    #             min = 0, max = 10, value = 1, step = 1),
-                                    # sliderInput("k2_1", "kcat (/s)",
-                                    #             min = 0, max = 100, value = 50, step = 1),
-                                    sliderInput("time_1", "Time(s)",
-                                                min = 1, max = 10000, value = 20, step = 1),
-                                    checkboxInput("theory_1", "Display Theoretical Values", value = FALSE),
-                                    hr()
+                                    htmlOutput("instruction_Pt")
                                   ),
                                   mainPanel(
                                     plotOutput("graph_Pt_1", height = "500px"),
-                                    tableOutput("table_Pt_1")
-                                  )),
+                                    fluidRow(
+                                      column(4, offset = 1,
+                                             wellPanel(
+                                               selectInput("e_1", "[E] (M)",
+                                                           choices = list("0.1 M" = 1 * 10^(-1), "0.02 M" = 2 * 10^(-2), "100 μM" = 100 * 10^(-6), "10 μM" = 10 * 10^(-6)),
+                                                           selected = 2 * 10^(-2)),
+                                               sliderInput("s_1", "[S] (M)",
+                                                           min = 0, max = 10, value = 1, step = 0.01),
+                                               actionButton("add_s_1", "Do an experiment"),
+                                               sliderInput("time_1", "Time(s)",
+                                                           min = 1, max = 10000, value = 20, step = 10),
+                                               checkboxInput("theory_1", "Display Theoretical Values", value = FALSE)
+                                             )
+                                             ),
+                                      column(2, offset = 1,
+                                             tableOutput("table_Pt_1")
+                                             )
+                                    )
+                                   )),
                          
                          tabPanel("P-t Graph 2",
-                                  sidebarPanel(
-                                    sliderInput("e_2", "[E] (μM)",
-                                                min = 0, max = 20000, value = 10, step = 10),
-                                    sliderInput("s_2", "[S] (M)",
-                                                min = 0, max = 10, value = 1, step = 0.1),
-                                    actionButton("add_s_2", "Do another experiment with a different value of [S]"),
-                                    # sliderInput("km_2", "Km (M)",
-                                    #             min = 0, max = 10, value = 1, step = 1),
-                                    # sliderInput("k2_2", "kcat (/s)",
-                                    #             min = 0, max = 100, value = 50, step = 1),
-                                    sliderInput("time_2", "Time(s)",
-                                                min = 1, max = 3000, value = 20, step = 1),
-                                    checkboxInput("theory_2", "Display Theoretical Values", value = FALSE),
-                                    hr()
+                                  fluidRow(
+                                    column(3,
+                                           wellPanel(
+                                             selectInput("e_2", "[E] (M)",
+                                                         choices = list("0.1 M" = 1 * 10^(-1), "0.02 M" = 2 * 10^(-2), "100 μM" = 100 * 10^(-6), "10 μM" = 10 * 10^(-6)),
+                                                         selected = 2 * 10^(-2)),
+                                             sliderInput("s_2", "[S] (M)",
+                                                         min = 0, max = 10, value = 1, step = 0.01),
+                                             actionButton("add_s_2", "Do an experiment"),
+                                             sliderInput("time_2", "Time(s)",
+                                                         min = 1, max = 10000, value = 20, step = 10),
+                                             checkboxInput("theory_2", "Display Theoretical Values", value = FALSE)
+                                           )
+                                           ),
+                                    column(8,
+                                           plotOutput("graph_Pt_2", height = "500px")
+                                           )
                                   ),
-                                  mainPanel(
-                                    plotOutput("graph_Pt_2", height = "500px")
-                                  )),
+                                    fluidRow(
+                                      column(3, align = "center",
+                                             wellPanel(tableOutput("table_Pt_2"))
+                                             
+                                      ),
+                                      column(8,
+                                             htmlOutput("instruction_Pt_2")
+                                      )
+                                    )
+                                  ),
                          
                          tabPanel("P-t and MM 1",
                                   sidebarPanel(
-                                    sliderInput("e_3", "E (μM)",
-                                                min = 0, max = 20000, value = 20000, step = 10),
+                                    width = 3,
+                                    selectInput("e_3", "[E] (M)",
+                                                choices = list("0.1 M" = 1 * 10^(-1), "0.02 M" = 2 * 10^(-2), "100 μM" = 100 * 10^(-6), "10 μM" = 10 * 10^(-6)),
+                                                selected = 2 * 10^(-2)),
                                     sliderInput("s_3", "S (M)",
                                                 min = 0, max = 10, value = 1, step = 0.01),
-                                    actionButton("add_s_3", "Do another experiment with a different value of [S]"),
+                                    actionButton("add_s_3", "Do an experiment"),
                                     sliderInput("time_3", "Time (s)",
                                                 min = 0, max = 1000, value = 20, step = 1),
                                     checkboxInput("theory_3", "Display Theoretical Values", value = FALSE)
-                                    
                                   ),
                                   mainPanel(
                                     plotOutput("graph_Pt_3", height = "400px"),
